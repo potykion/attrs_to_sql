eca90bc8cdd95598c87471c912a60cb6931f14d4
potykion <potykion@gmail.com>
2018-07-15 16:08:35 +0300

    0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="attrs_to_sql",
-    version="0.0.2",
+    version="0.0.3",
     author="potykion",
     author_email="potykion@gmail.com",
     description="Convert attrs class to CREATE TABLE command.",
